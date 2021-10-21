f1b2a89ae7ccc576dc20d09c17f16eaa5e9fe16e
Minh <trongminhtbh@gmail.com>
2021-10-21 10:46:07 +0700

    add data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import shoes from "../assets/img/black.png"
-function Card() {
+
+function Card({listShoes}) {
     return (
         
     <div className="card-item d-flex flex-row">
@@ -10,26 +11,26 @@ function Card() {
         </div>
         <div className="card-content d-flex flex-column">
             <div className="card-content-header">
-                AIR MAX PEGASUS
+                {listShoes.name}
             </div>
             <div className="card-content-star star-rating">
-                <input type="radio" id="5-stars" name="rating" value="5" />
-                <label for="5-stars" class="star">&#9733;</label>
-                <input type="radio" id="4-stars" name="rating" value="4" />
-                <label for="4-stars" class="star">&#9733;</label>
-                <input type="radio" id="3-stars" name="rating" value="3" />
-                <label for="3-stars" class="star">&#9733;</label>
-                <input type="radio" id="2-stars" name="rating" value="2" />
-                <label for="2-stars" class="star">&#9733;</label>
-                <input type="radio" id="1-star" name="rating" value="1" />
-                <label for="1-star" class="star">&#9733;</label>
+                <input type="radio" id= {"5-stars-"+listShoes.id} name="rating" value="5" />
+                <label for={"5-stars-"+listShoes.id} class="star">&#9733;</label>
+                <input type="radio" id={"4-stars-"+listShoes.id} name="rating" value="4" />
+                <label for={"4-stars-"+listShoes.id} class="star">&#9733;</label>
+                <input type="radio" id={"3-stars-"+listShoes.id} name="rating" value="3" />
+                <label for={"3-stars-"+listShoes.id} class="star">&#9733;</label>
+                <input type="radio" id={"2-stars-"+listShoes.id} name="rating" value="2" />
+                <label for={"2-stars-"+listShoes.id} class="star">&#9733;</label>
+                <input type="radio" id={"1-stars-"+listShoes.id} name="rating" value="1" />
+                <label for={"1-stars-"+listShoes.id} class="star">&#9733;</label>
             </div>
 
             <div className="card-content-price">
-                Price: <span>14$</span>
+                Price: <span>{listShoes.price}</span>
             </div>
             <div className="card-content-desc">
-                Description: <span>Thoai mai khi chay</span>
+                Description: <span>{listShoes.desc}</span>
             </div>
             <button >Details</button>
         </div>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,70 @@ import React, { useEffect } from "react";
 import Card from "../components/Card"
 import SideBar from "./Sidebar";
 import BranchTitle from "../components/ShoesBranch";
+
+const shoesList1 = [
+     {
+        name: "AIR MAX PEGASUS",
+        id: 1,
+        price: 500000,
+        desc: "chay nhanh"
+    },
+    {
+        name: "AIR MAX PEGASUS 2",
+        id: 2,
+        price: 600000,
+        desc: "chay em"
+    }, 
+    {
+        name: "AIR MAX PEGASUS 3",
+        id: 3,
+        price: 400000,
+        desc: "chay tot"
+    }
+]
+
+const shoesList2 = [
+    {
+        name: "AIR MAX PEGASUS 4",
+        id: 4,
+        price: 500000,
+        desc: "chay nhanh"
+    },
+    {
+        name: "AIR MAX PEGASUS 5",
+        id: 5,
+        price: 600000,
+        desc: "chay em"
+    }, 
+    {
+        name: "AIR MAX PEGASUS 6",
+        id: 6,
+        price: 400000,
+        desc: "chay tot"
+    }
+]
+
+const shoesList3 = [
+    {
+        name: "AIR MAX PEGASUS 7",
+        id: 7,
+        price: 500000,
+        desc: "chay nhanh"
+    },
+    {
+        name: "AIR MAX PEGASUS 8",
+        id: 8,
+        price: 600000,
+        desc: "chay em"
+    }, 
+    {
+        name: "AIR MAX PEGASUS 9",
+        id: 9,
+        price: 400000,
+        desc: "chay tot"
+    }
+]
+
 function Product() {
     return (
         <div className="product-container">
@@ -9,24 +73,30 @@ function Product() {
             <div className="product-display">
                 <BranchTitle name = "Nike"/>
                 <div className="shoes-list">
-                    <Card />
-                    <Card />
-                    <Card />
+                    {
+                    shoesList1.map( shoes =>  {
+                        return <Card listShoes = {shoes} />
+                    })
+                    }
                 </div>
 
                 <BranchTitle name = "Adidas"/>
                 <div className="shoes-list">
-                    <Card />
-                    <Card />
-                    <Card />
+                    {
+                    shoesList2.map( shoes =>  {
+                        return <Card listShoes = {shoes} />
+                    })
+                    }
                 </div>
 
 
                 <BranchTitle name = "Others"/>
                 <div className="shoes-list">
-                    <Card />
-                    <Card />
-                    <Card />
+                    {
+                    shoesList3.map( shoes =>  {
+                        return <Card listShoes = {shoes} />
+                    })
+                    }
                 </div>
                 
             </div>
