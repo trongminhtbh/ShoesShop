8e6391a0072555955c3faa17df08ad3eedae4b59
nhattangminh71 <83396633+nhattangminh71@users.noreply.github.com>
2021-11-17 18:07:22 +0800

    fixed header issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {StoreProvider} from "./store";
+import { StoreProvider } from "./store";
+import { Admin } from "./pages/admin"
 import * as serviceWorker from "./serviceWorker";
+
 
 ReactDOM.render(
     <StoreProvider>
         <App />
     </StoreProvider>
-, document.getElementById("root")
+    , document.getElementById("root")
 );
 
 serviceWorker.register();
