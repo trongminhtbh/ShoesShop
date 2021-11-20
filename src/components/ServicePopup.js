bf5cb47e1e6d3810db76c0423a6d41a029dd3dbf
leequangminh227 <54878619+leequangminh227@users.noreply.github.com>
2021-11-20 21:50:07 +0700

    Update ServicePopup.js

--- src/components/ServicePopup.js
+++ src/components/ServicePopup.js
@@ -14,7 +14,8 @@ export default function Popup(props){
     const [receive, setReceive] = useState(false)
     const [date, setDate] = useState("")
     const [picked, setpicked] = useState(false)
-
+    const [shipfee, setShipfee] = useState(0)
+ 
 
     useEffect(() => {
         setAddress(props.user.address)
@@ -61,8 +62,27 @@ export default function Popup(props){
         }    
     }
 
-    function checkprice(x,y){
-        setPrice(parseInt(x)*20*parseInt(y))
+    function checkprice(x,y,recv,addr){
+        if(recv){
+            const url = `
+            https://apistg.ahamove.com/v1/order/estimated_fee?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhaGEiLCJ0eXAiOiJ1c2VyIiwiY2lkIjoiODQ5MDg4NDIyODAiLCJzdGF0dXMiOiJPTkxJTkUiLCJlb2MiOiJ0ZXN0QGdt
+YWlsLmNvbSIsIm5vYyI6IkRyaW5raWVzIFRlc3QgQWNjb3VudCIsImN0eSI6IlNHTiIsImFjY291bnRfc3RhdHVzIjoiQ
+UNUSVZBVEVEIiwiZXhwIjoxNjM3MDYwNjIwLCJwYXJ0bmVyIjoidGVzdF9rZXkiLCJ0eXBlIjoiYXBpIn0.
+0JcO9Pjag39247XB2hAjxivKyOjt2HeVQZgvwyh5tQ4&order_time=0&
+path=
+[{"address":"725 Hẻm số 7 Thành Thái, Phường 14, Quận 10, Hồ Chí Minh, Việt Nam"},
+{"address":"`+String(addr)+`"}]
+&service_id=SGN-BIKE&requests=[]
+`
+            const response = fetch(url)
+            .then((response) => response.json())
+            .catch((error) => console.log(error));
+            console.log(url)
+            console.log(response)
+            
+            setPrice(0)
+        }
+        setPrice(parseInt(x)*20*parseInt(y)+price)
     }
 
 
@@ -76,14 +96,14 @@ export default function Popup(props){
                     
                     <label className="lb">Choose service</label>
                     <div>
-                        <input className="radio" type="radio" checked = {props.pack==1&&"checked"&&!picked||pack==1} name="service_type" value="standard" onChange={() => {setPack(1);checkprice(pairs,1);setpicked(true)}}></input><label>Standard (20$)</label>
+                        <input className="radio" type="radio" checked = {props.pack==1&&"checked"&&!picked||pack==1} name="service_type" value="standard" onChange={() => {setPack(1);checkprice(pairs,1,receive,address);setpicked(true)}}></input><label>Standard (20$)</label>
                     </div>
                     <div>
-                        <input className="radio" type="radio" checked = {props.pack==2&&"checked"&&!picked||pack==2} name="service_type" value="advanced" onChange={() => {setPack(2);checkprice(pairs,2);setpicked(true)}}></input><label>Advanced (40$)</label>
+                        <input className="radio" type="radio" checked = {props.pack==2&&"checked"&&!picked||pack==2} name="service_type" value="advanced" onChange={() => {setPack(2);checkprice(pairs,2,receive,address);setpicked(true)}}></input><label>Advanced (40$)</label>
                     </div>
                     <label className="lb">Pairs of shoes</label>
 
-                    <select className = "sel" value={pairs} onChange={event => {setPairs(event.target.value);checkprice(event.target.value,!picked&&props.pack||picked&&pack)}}>           
+                    <select className = "sel" value={pairs} onChange={event => {setPairs(event.target.value);checkprice(event.target.value,!picked&&props.pack||picked&&pack,receive,address)}}>           
                         <option value={0}>0</option>
                         <option value={1}>1</option>
                         <option value={2}>2</option>
@@ -101,10 +121,10 @@ export default function Popup(props){
                     </div>
                     <label className="lb">Where to receive</label>
                         <div>
-                            <input className="radio" type="radio" name="rec" value="at home" onChange={() => setReceive(true)}></input><label>Home</label>
+                            <input className="radio" type="radio" name="rec" value="at home" onChange={() => {setReceive(true);checkprice(pairs,!picked&&props.pack||picked&&pack,true,address)}}></input><label>Home</label>
                         </div>
                         <div>
-                            <input className="radio" type="radio" name="rec" value="store" onChange={() => setReceive(false)}></input><label>Store</label>
+                            <input className="radio" type="radio" name="rec" checked={!receive&&"checked"} value="store" onChange={() => {setReceive(false);checkprice(pairs,!picked&&props.pack||picked&&pack,false,address)}}></input><label>Store</label>
                         </div>
                     {receive && 
                         <div>
@@ -113,7 +133,7 @@ export default function Popup(props){
                             name="name"
                             className = "ip"
                             value = {address}
-                            onChange={event => setAddress(event.target.value)}/> <br></br>
+                            onChange={event => {setAddress(event.target.value);checkprice(pairs,!picked&&props.pack||picked&&pack,receive,event.target.value)}}/> <br></br>
                         </div>}
                     
                     <label className="lb">Date</label>
@@ -131,6 +151,7 @@ export default function Popup(props){
                                                             setReceive(false)
                                                             setAddress(props.user.address)
                                                             setpicked(false)
+                                                            setShipfee(0)
                                                             props.setPopup(false)}}>Close</button>
                 </form> 
             </div>
